f3496fa4fed2cf13e53f30a8998357c195bf623b
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-09-19 18:47:43 +0300

    Release 0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.cart.stock',
-    version='0.1',
+    version='0.2',
     description="Adds stock content type.",
     long_description=long_description,
     classifiers=[
